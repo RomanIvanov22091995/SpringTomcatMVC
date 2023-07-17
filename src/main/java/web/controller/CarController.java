7519e034910aa204416a90c5f6e3667ef53aa625
Roman <Roman22091995@yandex.ru>
2023-07-17 13:52:11 +0300

    update(new requirements)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,23 +1,23 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import web.model.Car;
-import web.service.CarService;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Controller
 public class CarController {
+    private final CarServiceImpl myService;
+
+    @Autowired
+    public CarController(CarServiceImpl myService) {
+        this.myService = myService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue="5") int count, Model model) {
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -8,18 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CarServiceImpl implements CarService {
+    private static List<Car> carList;
+
+    static {
+        carList = new ArrayList<>();
+        carList.add(new Car("Lada", "Black", 2000));
+        carList.add(new Car("BMV", "White", 2001));
+        carList.add(new Car("Honda", "Green", 2002));
+        carList.add(new Car("Kia", "Yellow", 2003));
+        carList.add(new Car("UAZ", "Red", 2010));
+    }
+
 
     @Override
     public List<Car> carList(Integer count) {
-        List<Car> carList; {
-            carList = new ArrayList<>();
-            carList.add(new Car("Lada", "Black", 2000));
-            carList.add(new Car("BMV", "White", 2001));
-            carList.add(new Car("Honda", "Green", 2002));
-            carList.add(new Car("Kia", "Yellow", 2003));
-            carList.add(new Car("UAZ", "Red", 2010));
-        }
+
         return carList.stream().limit(count).collect(Collectors.toList());
     }
 }
